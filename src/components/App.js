c988e1efdd486b0f8c851f4a063777c23281756a
Rasmus Sjoerslev <badedyr@gmail.com>
2016-03-22 20:38:49 +0100

    Basic webpack functionality added with React support through babel

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div>Hello - i am App.js</div>
+    );
+  }
+
+});
+
+module.exports = App;
