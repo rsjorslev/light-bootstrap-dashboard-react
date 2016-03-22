0d95974304d70242fb9c15a6ebe934597b1a03ad
Rasmus Sjoerslev <badedyr@gmail.com>
2016-03-22 21:06:57 +0100

    Changed webpack.config.js to output bundle.js in the /src folder and changed the reference from /dist/assets in index.html to just bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry:  __dirname + "/src/index.js",
   output: {
-    path: __dirname + "/dist/assets",
+    path: __dirname + "/src",
     filename: "bundle.js"
   },
   module: {
