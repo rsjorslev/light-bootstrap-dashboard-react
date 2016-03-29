5fb96fb4b85e90dca539bfe7e0f60db77732ba79
Rasmus Sjoerslev <badedyr@gmail.com>
2016-03-29 20:41:22 +0200

    webpack conf working with dist and dev

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+require('bootstrap/dist/css/bootstrap.css');
+require('./styles/Roboto.css');
+require('./styles/light-bootstrap-dashboard.scss');
+require('pixeden-stroke-7-icon/pe-icon-7-stroke/dist/pe-icon-7-stroke.css');
+require('font-awesome/css/font-awesome.css');
+
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,12 +1,10 @@
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var ExtractTextPlugin= require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
   entry:  __dirname + "/src/index.js",
   output: {
-    path: __dirname + "/src",
+    path: __dirname + "/dist/assets",
     filename: "bundle.js",
     publicPath: "/assets/"
   },
@@ -19,23 +17,17 @@ module.exports = {
             loader: 'babel'
         },
         {
-            //postcss is not being used anymore after i used an offline version of Roboto font. kept as a reference
             test: /\.scss$/,
-            loader: 'style!css!postcss!sass'
+            loader: ExtractTextPlugin.extract('style', 'css!sass')
         },
         {
             test: /\.(png|jpg)$/,
             loader: 'url-loader!?limit=8192'
         },
-        /**
-         *  TODO:
-         *  check if the mimetype is correct or of something should be added to woff/woff2
-         *  besides woff/woff2 the other mimetypes looks right
-         */
         // These 5 loaders are for bootstrap fonts and the alternative is for pixeden icons
         {
             test: /\.css$/,
-            loader: 'style-loader!css-loader'
+            loader: ExtractTextPlugin.extract('style', 'css')
         },
         {
             test: /\.(eot|eot\?d7yf1v|eot\?v=\d+\.\d+\.\d+)$/,
@@ -50,41 +42,32 @@ module.exports = {
             loader: "url?limit=10000&mimetype=application/octet-stream"
         },
         {
-            // test: /\.svg$/,
             test: /\.(svg|svg\?d7yf1v|svg\?v=\d+\.\d+\.\d+)$/,
             loader: "url?limit=10000&mimetype=image/svg+xml"
         }
     ]
   },
 
+    plugins: [
+        // 
+        new ExtractTextPlugin("style.css"),
+
+        // optimizations
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+
+    ]
+
 /*
-not needed anymore as i have downloaded the Roboto font and created @font-face in ./styles/Roboto.css
     postcss: function (webpack) {
         return [
             require("postcss-font-magician")
         ]
-    },
+    }
 */
-
-/*
-dont think i need this for the WDS - only relevant for production/dist?
-    plugins: [
-        new HtmlWebpackPlugin({
-            // inject: false,
-            template: './src/index.ejs',
-            appMountId: 'app'
-        })
-    ],
-*/
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-
-  devServer: {
-    contentBase: "./src",
-    colors: true,
-    historyApiFallback: true,
-    inline: true,
-    hot: true
-  }
 };
